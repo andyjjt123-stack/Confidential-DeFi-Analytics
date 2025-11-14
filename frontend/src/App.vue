5bde0e321f18769efe2ee655b94a5a7e10495cd2
andyjjt123-stack <andyjjt123@gmail.com>
2025-11-14 16:03:52 +0800

    logo update
    logo update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,25 +5,20 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <h1>ZAMA Confidential Vault</h1>
   </header>
-
-  <RouterView />
+  <router-view />
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
   max-height: 100vh;
+}
+
+header h1 {
+  text-align: center;
+  color: #2b2b2b;
 }
 
 .logo {
