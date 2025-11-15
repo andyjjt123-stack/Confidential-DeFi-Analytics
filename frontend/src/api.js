9bd6f3c3fd0a19d39f54719e49e21214a31c33a5
andyjjt123-stack <andyjjt123@gmail.com>
2025-11-15 21:06:31 +0800

    change api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,12 +1,10 @@
-// import axios from "axios";
-// 臨時硬寫：先確保打得到後端
-// const base = "https://cda-backend-499q.onrender.com";
-// export default axios.create({ baseURL: base });
-
 import axios from "axios";
-
-// 本地開發（Vite 代理）可讓它為空字串；上線則由打包時的變數帶入
-const base = import.meta.env.VITE_API_BASE || "";
-
+// 臨時硬寫：先確保打得到後端
+const base = "https://cda-backend-499q.onrender.com";
 export default axios.create({ baseURL: base });
+
+//import axios from "axios";
+// 本地開發（Vite 代理）可讓它為空字串；上線則由打包時的變數帶入
+//const base = import.meta.env.VITE_API_BASE || "";
+//export default axios.create({ baseURL: base });//
 
