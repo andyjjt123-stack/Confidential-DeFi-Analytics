8c1ba2c3a2bfdeab8c5fc9626d361529f05be686
andyjjt123-stack <andyjjt123@gmail.com>
2025-11-17 15:54:26 +0800

    add cloudflare support

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,9 +10,14 @@ export default defineConfig({
     },
   },
   server: {
+    host: '0.0.0.0',                 // 對外開放（給 tunnel 用）
+    port: 5173,                      // 你 cloudflared 連的那個 port
+    allowedHosts: [
+      'kids-northern-voices-arbitrary.trycloudflare.com', // cloudflared 給你的 host
+    ],
     proxy: {
       '/vault': { target: 'http://localhost:8080', changeOrigin: true },
       '/fhe':   { target: 'http://localhost:8080', changeOrigin: true },
-	}
-  }
+    },
+  },
 })
